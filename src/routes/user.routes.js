91fa91fadaf3cfb468a109c10a87aa83d0ab7e30
anuj-thakur-513 <anujthakur2003.anuj@gmail.com>
2024-03-31 18:40:13 +0530

    add routes for update user details

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
@@ -30,5 +35,20 @@ userRouter.post("/login", loginUser);
 // secured routes
 userRouter.post("/logout", verifyJwt, logoutUser);
 userRouter.post("/refresh-token", refreshAccessToken);
+userRouter.post("/update-password", verifyJwt, changeCurrentPassword);
+userRouter.post("/get-current-user", verifyJwt, getCurrentUser);
+userRouter.post("/update-account-details", verifyJwt, updateAccountDetails);
+userRouter.post(
+  "/update-avatar",
+  verifyJwt,
+  upload.single("avatar"),
+  updateUserAvatar
+);
+userRouter.post(
+  "/update-cover-image",
+  verifyJwt,
+  upload.single("coverImage"),
+  updateUserCoverImage
+);
 
 export default userRouter;
